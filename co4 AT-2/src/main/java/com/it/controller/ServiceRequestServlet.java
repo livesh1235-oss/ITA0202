98129fc133648a33fb417ec92c6c7f3160a03d43
livesh1235-oss <liveshvetrivel@gmail.com>
2026-09-01 13:30:00 +0530

    Update co4 AT-2 with compiled classes and Tomcat deployment

--- co4 AT-2/src/main/java/com/it/controller/ServiceRequestServlet.java
+++ co4 AT-2/src/main/java/com/it/controller/ServiceRequestServlet.java
@@ -3,11 +3,11 @@ package com.it.controller;
 import com.it.model.ServiceRequest;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Controller Servlet for processing IT Service Requests.
